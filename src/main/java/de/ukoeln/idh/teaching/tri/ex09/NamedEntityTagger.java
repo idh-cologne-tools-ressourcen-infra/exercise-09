7919ae3be0b4adb8a7e7df84b3495187deefcecb
kekpja <56304716+kekpja@users.noreply.github.com>
2021-02-15 20:05:51 +0100

    kek

--- src/main/java/de/ukoeln/idh/teaching/tri/ex09/NamedEntityTagger.java
+++ src/main/java/de/ukoeln/idh/teaching/tri/ex09/NamedEntityTagger.java
@@ -1,26 +1,35 @@
 package de.ukoeln.idh.teaching.tri.ex09;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
-public class NamedEntityTagger extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
+public class NamedEntityTagger extends JCasAnnotator_ImplBase {
 
 	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		// iterate over tokens
-				for (Token token : aJCas.select(Token.class)) {
-					// check first character
-					char first = token.getCoveredText().charAt(0);
-					if (Character.isUpperCase(first)) {
-						// create a new NamedEntity annotation
-						NamedEntity ne = new NamedEntity(aJCas, token.getBegin(), token.getEnd());
-						// set feature value to PER
-						ne.setValue("PER");
-					}
-				}
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		// iterate over all tokens
+		for (Token token : jcas.select(Token.class)) {
+
+			// get first character of the token
+			char firstCharacter = token.getCoveredText().charAt(0);
+
+			// check whether it's upper case
+			if (Character.isUpperCase(firstCharacter)) {
+
+				// create a new NamedEntity annotation
+				NamedEntity ne = new NamedEntity(jcas, token.getBegin(), token.getEnd());
+
+				// set a feature value to PER
+				ne.setValue("PER");
+
+				// add the feature structure to CAS indexes
+				ne.addToIndexes();
+			}
+		}
 	}
 
 }
